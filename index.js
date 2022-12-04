e313be72dab19c0d7d03a8e1e70a8a1d2338d40e
Su_Yang_233 <100500860+Scrohild@users.noreply.github.com>
2022-12-05 07:22:16 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// _1: Xomu, KiraraMagic, Kumi-P等合集
+var _0_0 = "http://music.163.com/song/media/outer/url?id=1907498364.mp3"
+var _0_1 = "";
+var _0_2 = "";
+var _0_3 = "";
+var _0_4 = "";
+var _0_5 = "";
+var _0_6 = "";
+var _0_7 = "";
+var _0_8 = "";
+var _0_9 = "";
+var _0_10 = "";
+
+var _0 = [
+	_0_0, _0_1, _0_2, _0_3, _0_4,
+	_0_5, _0_6, _0_7, _0_8, _0_9,
+	_0_10,
+];
+var _1 = [];
+var _2 = [];
+var _3 = [];
+
+var _index = [_0, _1, _2, _3];
+function play(list, music){
+	var _list = _index[list];
+	var _link = _list[music];
+}
